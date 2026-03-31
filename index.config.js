5813df65d94127cdc81a3339387c872e19b6fa7b
♡｡Eternity.✨ <jierryx8@gmail.com>
2026-03-31 12:58:55 +0800

    Refactor index.config.js structure and update entries

--- index.config.js
+++ index.config.js
@@ -16,7 +16,6 @@ var __copyProps = (to, from, except, desc) => {
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
-// src/index.config.js
 var index_config_exports = {};
 __export(index_config_exports, {
   default: () => index_config_default
@@ -24,108 +23,65 @@ __export(index_config_exports, {
 module.exports = __toCommonJS(index_config_exports);
 
 var index_config_default = {
-  ali: {
-    token: "",
-    token280: "token280"
-  },
-  quark: {
-    cookie: ""
-  },
-  uc: {
-    cookie: "cookie",
-    token: "token",
-    ut: "ut"
-  },
-  y115: {
-    cookie: ""
-  },
-  muou: {
-    url: ""
-  },
-  wogg: {
-    url: ""
-  },
-  leijing: {
-    url: ""
-  },
-  tgsou: {
-    tgPic: false,
-    count: 0,
-    url: "",
-    channelUsername: ""
-  },
+  ali: { token: "", token280: "token280" },
+  quark: { cookie: "" },
+  uc: { cookie: "cookie", token: "token", ut: "ut" },
+  y115: { cookie: "" },
+  muou: { url: "" },
+  wogg: { url: "" },
+  leijing: { url: "" },
+  tgsou: { tgPic: false, count: 0, url: "", channelUsername: "" },
   tgchannel: {},
-  sites: {
-    list: []
-  },
-  pans: {
-    list: []
-  },
+  sites: { list: [] },
+  pans: { list: [] },
+
   danmu: {
     urls: [
-      { address: "https://logdanmu.dpdns.org", name: "Eternity默认1" },
-      { address: "https://fjj0417.dpdns.org/87654321", name: "Eternity默认2" }, 
-      { address: "https://dm.stardm.us.kg:443/87654321", name: "Eternity默认3" }, 
-      { address: "https://danmu.14812910.xyz/87654321", name: "Eternity默认4" }, 
-      { address: "https://313236.xyz/87654321", name: "Eternity默认5" }
+      { address: "https://logdanmu.dpdns.org", name: "♡｡Eternity.✨ 默认1" },
+      { address: "https://fjj0417.dpdns.org/87654321", name: "♡｡Eternity.✨ 默认2" },
+      { address: "https://dm.stardm.us.kg:443/87654321", name: "♡｡Eternity.✨ 默认3" },
+      { address: "https://danmu.14812910.xyz/87654321", name: "♡｡Eternity.✨ 默认4" },
+      { address: "https://313236.xyz/87654321", name: "♡｡Eternity.✨ 默认5" }
     ],
     autoPush: true
   },
-  t4: {list: [
-    {name: "✈️ Eternity TG频道",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=独播库"},
-    {name: "📡 Eternity IPTV直播",
-      address: "https://iptv.jsnzkpg.uk"},
-    {name: "🐯 Eternity 虎牙直播",
-      address: "https://php.doube.eu.org/spider/php/虎牙直播.php"},
-    {name: "🅱️ Eternity 哔哩直播",
-      address: "https://bilibili.jsnzkpg.uk"},
-    {name: "🎞️ Eternity 影视爬虫",
-      address: "https://dy.7772888.xyz/api.php/tvbox"},
-    {name: "🍟 Eternity Emby嗅探",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=emby"},
-    {name: "⚡️ Eternity VIP嗅探",
-      address: "https://php.doube.eu.org/spider/php/官方资源.php"},
-    {name: "🍁 Eternity 非凡PHP",
-      address: "https://php.doube.eu.org/spider/php/非凡资源.php"},
-    {name: "🐣 Eternity 天堂PHP",
-      address: "https://php.doube.eu.org/spider/php/电影天堂.php"},
-    {name: "💧 Eternity 欧乐PHP",
-      address: "https://php.doube.eu.org/spider/php/欧乐影院.php"},
-    {name: "🦄 Eternity U视影视",
-      address: "http://zhangqun1818.serv00.net:5052/?sp=UVod"},
-    {name: "🥔 Eternity 土豆影视",
-      address: "http://zhangqun1818.serv00.net:5052/?sp=土豆浏览器"},
-    {name: "🐈‍⬛ Eternity 七猫短剧",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=七猫短剧"},
-    {name: "🍚 Eternity 西饭短剧",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=西饭短剧"},
-    {name: "🦛 Eternity 河马短剧",
-      address: "http://zhangqun1818.serv00.net:5052/?sp=河马短剧"},
-    {name: "🇨🇳 Eternity 央视栏目",
-      address: "https://catbox.n13.club/18/央视影视.php"},
-    {name: "📻 Eternity 凤凰FM",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=凤凰fm"},
-    {name: "🐰 Eternity 兔贝少儿",
-      address: "http://zhangqun1818.serv00.net:6628/?spider=兔小贝"}
-  ]},
-  cms: {list: [
-    {name: "🌪️ Eternity 暴风采集",
-     address: "http://by.bfzyapi.com/api.php/provide/vod"},
-    {name: "🪐 Eternity iQiYi采集",
-     address: "https://iqiyizyapi.com/api.php/provide/vod"},
-  ]},
+
+  t4: {
+    list: [
+      {name: "✈️ ♡｡Eternity.✨ TG频道", address: "http://zhangqun1818.serv00.net:6628/?spider=独播库"},
+      {name: "📡 ♡｡Eternity.✨ IPTV直播", address: "https://iptv.jsnzkpg.uk"},
+      {name: "🐟 ♡｡Eternity.✨ 斗鱼直播", address: "https://php.doube.eu.org/spider/php/斗鱼直播.php"},
+      {name: "🐯 ♡｡Eternity.✨ 虎牙直播", address: "https://php.doube.eu.org/spider/php/虎牙直播.php"},
+      {name: "🅱️ ♡｡Eternity.✨ 哔哩直播", address: "https://bilibili.jsnzkpg.uk"},
+      {name: "🎞️ ♡｡Eternity.✨ 影视爬虫", address: "https://dy.7772888.xyz/api.php/tvbox"},
+      {name: "▶️ ♡｡Eternity.✨ Emby嗅探", address: "http://zhangqun1818.serv00.net:6628/?spider=emby"},
+      {name: "🍁 ♡｡Eternity.✨ 非凡PHP", address: "https://php.doube.eu.org/spider/php/非凡资源.php"},
+      {name: "🐣 ♡｡Eternity.✨ 天堂PHP", address: "https://php.doube.eu.org/spider/php/电影天堂.php"},
+      {name: "💧 ♡｡Eternity.✨ 欧乐PHP", address: "https://php.doube.eu.org/spider/php/欧乐影院.php"},
+      {name: "🦄 ♡｡Eternity.✨ U视影视", address: "http://zhangqun1818.serv00.net:5052/?sp=UVod"},
+      {name: "🥔 ♡｡Eternity.✨ 土豆影视", address: "http://zhangqun1818.serv00.net:5052/?sp=土豆浏览器"},
+      {name: "🐈‍⬛ ♡｡Eternity.✨ 七猫短剧", address: "http://zhangqun1818.serv00.net:6628/?spider=七猫短剧"},
+      {name: "🍚 ♡｡Eternity.✨ 西饭短剧", address: "http://zhangqun1818.serv00.net:6628/?spider=西饭短剧"},
+      {name: "🦛 ♡｡Eternity.✨ 河马短剧", address: "http://zhangqun1818.serv00.net:5052/?sp=河马短剧"},
+      {name: "🇨🇳 ♡｡Eternity.✨ 央视栏目", address: "https://catbox.n13.club/18/央视影视.php"},
+      {name: "📻 ♡｡Eternity.✨ 凤凰FM", address: "http://zhangqun1818.serv00.net:6628/?spider=凤凰fm"},
+      {name: "🐰 ♡｡Eternity.✨ 兔贝少儿", address: "http://zhangqun1818.serv00.net:6628/?spider=兔小贝"}
+    ]
+  },
+
+  cms: {
+    list: [
+      {name: "🌪️ ♡｡Eternity.✨ 暴风采集", address: "http://by.bfzyapi.com/api.php/provide/vod"},
+      {name: "🪐 ♡｡Eternity.✨ iQiYi采集", address: "https://iqiyizyapi.com/api.php/provide/vod"}
+    ]
+  },
+
   alist: [
-    {
-      name: "🐉 Eternity 神族九帝",
-      server: "https://alist.shenzjd.com"
-    },
-    {
-      name: "💢 Eternity repl",
-      server: "https://ali.liucn.repl.co"
-    }
+    { name: "🐉 ♡｡Eternity.✨ 神族九帝", server: "https://alist.shenzjd.com" },
+    { name: "💢 ♡｡Eternity.✨ repl", server: "https://ali.liucn.repl.co" }
   ],
+
   color: [ /* 颜色配置保持不变 */ 
-    // ...（颜色部分代码太长，未改动，保持原样）
+    // ...（颜色部分代码太长，这里保留原样，你可以不改）
   ]
 };
